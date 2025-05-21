c53ed59b37aa78dfa4cffc4d85adac8d2cd5f7ec
Dreng0t <louischicot4@gmail.com>
2025-05-21 13:47:23 +0100

    implemented a profile page wich renders the UserDetails component which fetches the info for the currently logged in user, from AuthContext, and renders them to the screen

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Characters from './pages/Characters';
 import Items from './pages/Items';
+import Profile from './pages/Profile';
 
 //import Items from './pages/Items';
 
@@ -20,6 +21,7 @@ function App() {
         <Route path="/items" element={<Items />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </div>
   );
