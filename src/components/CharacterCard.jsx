19b0f248bd41870fe4769c681a243194c271e172
Midas van Soest <midasvsoest@gmail.com>
2025-05-21 16:26:12 +0200

    Feat: Add Character details

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
-import '../CharacterCard.css'
+import { useNavigate } from 'react-router-dom';
+import '../CharacterCard.css';
 
 const CharacterCard = ({ character }) => {
-    return (
-        <div className="character-card">
-            <h2>{character.name}</h2>
-            <p><strong>Race:</strong> {character.race}</p>
-            <p><strong>Level:</strong> {character.level}</p>
-            <p><strong>Class:</strong> {character.classes.map(c => `${c.name} ${c.level}`).join(', ')}</p>
-            <p><strong>STR:</strong> {character.stats.strength}</p>
-        </div>
-    );
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/characters/${character._id}`);
+  };
+
+  return (
+    <div className="character-card" onClick={handleClick} style={{ cursor: 'pointer' }}>
+      <h2>{character.name}</h2>
+      <p><strong>Race:</strong> {character.race}</p>
+      <p><strong>Level:</strong> {character.level}</p>
+      <p><strong>Class:</strong> {character.classes.map(c => `${c.name} ${c.level}`).join(', ')}</p>
+      <p><strong>STR:</strong> {character.stats.strength}</p>
+    </div>
+  );
 };
 
 export default CharacterCard;
