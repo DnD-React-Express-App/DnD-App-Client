f58c6f537a0c06229b7b81e121a8b85904852f2f
Dreng0t <louischicot4@gmail.com>
2025-05-21 13:35:47 +0100

    implemented a link to log out which will conditionally render in place of the log in link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,31 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../context/auth.context";
 
 function Navbar() {
+  const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
+  const navigate = useNavigate();
 
+  const handleLogout = (e) => {
+    e.preventDefault(); 
+    logOutUser();
+    navigate('/');
+  };
 
   return (
-    <nav className={"Navbar"}>
+    <nav className="Navbar">
       <div>
-        <NavLink to="/"> Home </NavLink>
-        <NavLink to="/characters"> Characters </NavLink>
-        <NavLink to="/items"> Items </NavLink>
-        <NavLink to="/profile"> Profile </NavLink>
-        <NavLink to="/login"> Login </NavLink>
+        <NavLink to="/">Home</NavLink>
+        {isLoggedIn && <NavLink to="/characters">Characters</NavLink>}
+        {isLoggedIn && <NavLink to="/items">Items</NavLink>}
+        {isLoggedIn && <NavLink to="/profile">Profile</NavLink>}
+        {!isLoggedIn && <NavLink to="/login">Login</NavLink>}
+        {!isLoggedIn && <NavLink to="/signup">Sign Up</NavLink>}
+        {isLoggedIn && (
+          <NavLink to="/" onClick={handleLogout}>
+            Log Out
+          </NavLink>
+        )}
       </div>
     </nav>
   );
