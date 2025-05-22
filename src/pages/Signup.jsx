4fcab6c77006290ddd354aa4561e4beb73ac37c0
Dreng0t <louischicot4@gmail.com>
2025-05-22 09:30:16 +0100

    fixed the newly modularised signup functionality to now also automatically log in the user after sign up

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,13 +1,17 @@
 import { useNavigate } from 'react-router-dom';
 import { signup } from '../services/auth.service';
 import UserForm from '../components/UserForm';
+import { useContext } from 'react';
+import { AuthContext } from '../context/auth.context';
 
 function Signup() {
+  const { storeToken, authenticateUser } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const handleSignup = (formData) => {
     return signup(formData).then(res => {
       localStorage.setItem('authToken', res.data.authToken);
+      authenticateUser();
       navigate('/profile');
     });
   };
